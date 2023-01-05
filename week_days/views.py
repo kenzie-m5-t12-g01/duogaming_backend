66490ddabfeffb8b954d7d6182b1f949c171ac77
Gabriel Alencar <gabrielalencar33564@gmail.com>
2023-01-05 13:20:26 -0300

    fix: pagination, page-size = 7

--- week_days/views.py
+++ week_days/views.py
@@ -1,17 +1,19 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from week_days.serializers import WeekDaySerializer
 from week_days.models import WeekDay    
 from week_days.permissions import IsSuperUserOrListOnly
-from ads.serializers import AdSerializer
+from rest_framework.pagination import PageNumberPagination
 
+class WeekDayPagination(PageNumberPagination):
+    page_size = 7
 
 class WeekDayView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsSuperUserOrListOnly]
     serializer_class = WeekDaySerializer
     queryset = WeekDay.objects.all()
+    pagination_class = WeekDayPagination
     
 
 class WeekDayDetailView(generics.RetrieveUpdateDestroyAPIView):
