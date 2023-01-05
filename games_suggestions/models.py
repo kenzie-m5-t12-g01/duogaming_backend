9eed003b7e1f892c1464844b7fe012ab21fec56c
Bruna <bruna.dall9741@gmail.com>
2023-01-05 11:03:37 -0300

    feat: create game suggestion model

--- games_suggestions/models.py
+++ games_suggestions/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class GameSuggestion(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    game_title = models.CharField(max_length=120)
+    observations = models.TextField(null=True, blank=True, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="games_suggestions",
+    )
+
+    class Meta:
+        ordering = "created_at",
