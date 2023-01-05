6abaf551c3417b22dd0ba0224d5398812d8eccbb
Gabriel Alencar <gabrielalencar33564@gmail.com>
2023-01-04 23:37:29 -0300

    feat: urls app ad and app week-day

--- duogaming/urls.py
+++ duogaming/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
+    path("api/", include("week_days.urls")),
+    path("api/", include("ads.urls")),
 ]
