a394908a57ee8c5778d72f0e1b749a94652faba6
Gabriel Alencar <gabrielalencar33564@gmail.com>
2023-01-05 17:33:08 -0300

    fix: resolving week_days

--- ads/serializers.py
+++ ads/serializers.py
@@ -2,35 +2,37 @@ from rest_framework import serializers
 from ads.models import Ad
 from week_days.serializers import WeekDaySerializer
 from week_days.models import WeekDay
-import ipdb
+
+class WeekDaySerializerSerializer(serializers.Serializer):
+    day = serializers.CharField()
 
 class AdSerializer(serializers.ModelSerializer):
-
-    week_days = WeekDaySerializer(many=True)
-    # game_title = serializers.SerializerMethodField()
+    week_days = WeekDaySerializerSerializer(many=True)
 
     class Meta:
         model = Ad
         fields = [
             "id",
             "nickname",
-            # "game_title"
             "years_playing",
             "discord_user",
             "day_period",
             "week_days",
+            "user_id",
+            "game_id",
             "created_at",
             "updated_at",
         ]
-        read_only_fields = ["id", "created_at", "updated_at",]
-
-
-    # def get_game_title(self, obj: Ad):
-    #     return obj.game.game_title
+        read_only_fields = [
+            "id", 
+            "created_at", 
+            "updated_at", 
+            "user_id", 
+            "game_id",
+        ]
 
     def create(self, validated_data: dict) -> Ad:
         week_days = validated_data.pop("week_days")
-        ipdb.set_trace()
 
         ad = Ad.objects.create(**validated_data)
 
@@ -39,6 +41,5 @@ class AdSerializer(serializers.ModelSerializer):
             ad.week_days.add(day_obj)
 
         ad.save()
-
         return ad
             
