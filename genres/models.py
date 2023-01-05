f02c9542c550a11407bf06bd2b7a50d5fed15cc5
terragabriella <gabriellaterra8@gmail.com>
2023-01-05 12:58:29 -0300

    fix: ordering class Meta genre model

--- genres/models.py
+++ genres/models.py
@@ -4,7 +4,7 @@ import uuid
 
 class Genre (models.Model):
     class Meta:
-        ordering = ("name")
+        ordering = "name",
 
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=120, unique=True, error_messages={
