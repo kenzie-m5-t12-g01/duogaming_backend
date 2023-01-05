bdf9226418c0713c372cb500e11ffa74cd9247aa
Bruna <bruna.dall9741@gmail.com>
2023-01-05 11:04:08 -0300

    feat: create game suggestion serializer

--- games_suggestions/serializers.py
+++ games_suggestions/serializers.py
@@ -1,1 +1,13 @@
+from rest_framework import serializers
 
+from games_suggestions.models import GameSuggestion
+
+
+class GameSuggestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GameSuggestion
+        fields = "__all__"
+        read_only_fields = ["id", "user_id"]
+
+    def create(self, validated_data: dict) -> GameSuggestion:
+        return GameSuggestion.objects.create(**validated_data)
