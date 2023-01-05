d3b4608802c18ac344bf9dec6e5c7a645bdf8663
Bruna <bruna.dall9741@gmail.com>
2023-01-05 11:14:09 -0300

    feat: create game suggestion views

--- games_suggestions/views.py
+++ games_suggestions/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAdminUser
+from rest_framework import generics
 
-# Create your views here.
+from games_suggestions.models import GameSuggestion
+from games_suggestions.serializers import GameSuggestionSerializer
+from users.permissions import IsSuperUserOrPostOnly
+
+
+class GameSuggestionView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsSuperUserOrPostOnly]
+
+    queryset = GameSuggestion.objects.all()
+    serializer_class = GameSuggestionSerializer
+
+
+class GameSuggestionDetailView(generics.RetrieveDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
+
+    queryset = GameSuggestion.objects.all()
+    serializer_class = GameSuggestionSerializer
