68e7ab31f35a42ec157d8694acbf99a69e143c1f
Gabriel Alencar <gabrielalencar33564@gmail.com>
2023-01-06 17:42:47 -0300

    fix: filtes user ads and game ads

--- week_days/views.py
+++ week_days/views.py
@@ -2,20 +2,12 @@ from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from week_days.serializers import WeekDaySerializer
 from week_days.models import WeekDay    
-from week_days.permissions import IsSuperUserOrListOnly
+from rest_framework.permissions import IsAuthenticated
 from week_days.pagination import WeekDayPagination
 
-class WeekDayView(generics.ListCreateAPIView):
+class WeekDayListView(generics.ListAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsSuperUserOrListOnly]
+    permission_classes = [IsAuthenticated]
     serializer_class = WeekDaySerializer
     queryset = WeekDay.objects.all()
     pagination_class = WeekDayPagination
-    
-
-class WeekDayDetailView(generics.RetrieveUpdateDestroyAPIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsSuperUserOrListOnly]
-    serializer_class = WeekDaySerializer
-    queryset = WeekDay.objects.all()
-    lookup_url_kwarg = "id"
