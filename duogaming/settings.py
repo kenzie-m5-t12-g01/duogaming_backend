c37771f5a7d2ad7d73a6867c3a7554677b822933
Gabriel Alencar <gabrielalencar33564@gmail.com>
2023-01-09 20:08:57 -0300

    deploy

--- duogaming/settings.py
+++ duogaming/settings.py
@@ -67,7 +67,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
